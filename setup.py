a431c4448a8bb38777a2b729d5c2a139eba58f4c
Pragyan <pragyan0814@gmail.com>
2026-03-29 22:07:37 +0545

    project baseline setup is done

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Pragyan",
+    author_email="pragyan0814@gmail.com",
+    packages=find_packages()
+)
